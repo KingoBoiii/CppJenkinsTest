abf8eb00aa453f388d913a11f894d6a8120f53ee
KingoBoiii <kingodk@outlook.dk>
2021-08-13 18:12:58 +0200

    Added a new line...

--- JenkinsTest/Source/main.cpp
+++ JenkinsTest/Source/main.cpp
@@ -6,6 +6,7 @@ int main()
 	std::cout << "Test line 2" << std::endl;
 	std::cout << "Test line 3" << std::endl;
 	std::cout << "Test line 4" << std::endl;
+	std::cout << "Final test..." << std::endl;
 	return 0;
 }
 
