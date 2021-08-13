92f379f17bc55a156ac07b538da6b1c77527b715
KingoBoiii <kingodk@outlook.dk>
2021-08-13 19:22:00 +0200

    Test

--- JenkinsTest/Source/main.cpp
+++ JenkinsTest/Source/main.cpp
@@ -12,6 +12,7 @@ int main()
 	std::cout << "Hello, world!" << std::endl;
 	std::cout << "Hello, world2!" << std::endl;
 	std::cout << "Hello, world2!" << std::endl;
+	std::cout << "VIRK!!!!!!!!!!!!!!" << std::endl;
 	return 0;
 }
 
